4c97a90e6aba6d2cf7041da8ab44cce2257af36e
Ingólfur Orri <ingolfurorri@gmail.com>
2019-09-17 10:21:46 +0000

    sda

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,10 +1,24 @@
 pos_x = 1
 pos_y = 1
 
-#Kemst alltaf norður nema y=3 eða x==y
-#Kemst alltaf suður nema y=1 eða (x==2, y==3)
-#Kemst alltaf austir þegar y==x og y==3 (Eða x < y)
-#Kemst alltaf vestur í (2,3), (3,3) og (2,2)
+def movement_available(x, y):
+    movement = ''
+    if not (y == 3 or x == 2 and y == 2):
+        movement += '(N)orth'
+    if (y == 3 and y != x):
+        movement += '(E)ast'
+    if not (y == 1 or x == 2 and y == 3):
+        movement += '(S)outh'
+    if (x == 2 and y == 3 or x == y and x != 1 or x == 1 and y == 2):
+        movement += '(W)est'
+    return movement
 
+def move(pos_current_x, pos_current_y, movement):
+    
 
-def move(pos_x, pos_y):
+pos_current_x = pos_x
+pos_current_y = pos_y
+
+while not (pos_current_x == 3 and pos_current_y == 1):
+    print(movement_available(pos_current_x, pos_current_y))
+    move_user = input('Input where to go: ')
