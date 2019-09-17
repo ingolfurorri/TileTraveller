757179042f29533109bac80039bd4ce273ad21f1
Ingólfur Orri <ingolfurorri@gmail.com>
2019-09-17 09:22:31 +0000

    algo

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,10 @@
-print("Guðjón er bitchass")
+pos_x = 1
+pos_y = 1
+
+#Kemst alltaf norður nema y=3 eða x==y
+#Kemst alltaf suður nema y=1 eða (x==2, y==3)
+#Kemst alltaf austir þegar y==x og y==3 (Eða x < y)
+#Kemst alltaf vestur í (2,3), (3,3) og (2,2)
+
+
+def move(pos_x, pos_y):
