74d09dacfe66bbb526d8a3b3dc539fee42556483
Ingólfur Orri <ingolfurorri@gmail.com>
2019-09-17 11:14:32 +0000

    working, needs beauty

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,18 +1,44 @@
 pos_x = 1
 pos_y = 1
 
+def move_al(move_user, move_allowed):
+    move_user = move_user.lower()
+    for i in move_allowed:
+        if move_user == i:
+            return True
+
+    return False
+
 def movement_available(x, y):
     movement = ''
+    move_yes = ''
     if not (y == 3 or x == 2 and y == 2):
-        movement += '(N)orth'
-    if (y == 3 or y != x and y != 1 and x != 2):
-        movement += '(E)ast'
+        if(movement == ''):
+            movement += '(N)orth'
+            move_yes += 'n'
+    if (x < y):
+        if(movement == ''):
+            movement += '(E)ast'
+            move_yes += 'e'
+        else:
+            movement += ' or (E)ast'
+            move_yes += 'e'
     if not (y == 1 or x == 2 and y == 3):
-        movement += '(S)outh'
+        if(movement == ''):
+            movement += '(S)outh'
+            move_yes += 's'
+        else:
+            movement += ' or (S)outh'
+            move_yes += 's'
     if (x == 2 and y == 3 or x == y and x != 1):
-        movement += '(W)est'
+        if(movement == ''):
+            movement += ' (W)est'
+            move_yes += 'w'
+        else:
+            movement += ' or (W)est'
+            move_yes += 'w'
 
-    return movement
+    return movement, move_yes
 
 def move(x, y, move_user):
     movement = move_user.lower()
@@ -27,12 +53,25 @@ def move(x, y, move_user):
 
     return x, y
 
+#Main program
+
 pos_current_x = pos_x
 pos_current_y = pos_y
 
 while not (pos_current_x == 3 and pos_current_y == 1):
-    print(movement_available(pos_current_x, pos_current_y))
-    move_user = input('Input where to go: ')
-    
-    pos_current_x, pos_current_y = move(pos_current_x, pos_current_y, move_user) 
-    print(pos_current_x, pos_current_y)
+    move_avail , move_allowed = movement_available(pos_current_x, pos_current_y)
+    move_avail += '.'
+    print("You can travel:", move_avail)
+    move_user = input("Direction: ")
+
+    move_bool = move_al(move_user, move_allowed)
+
+    while not(move_bool):
+        print("Not a valid direction!")
+        move_user = input("Direction: ")
+        move_bool = move_al(move_user, move_allowed)
+
+    pos_current_x, pos_current_y = move(pos_current_x, pos_current_y, move_user)
+
+else:
+    print("Victory!")
