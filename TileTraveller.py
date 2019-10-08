60dd6614dea761ff82e327060028e5e3e896e574
Ingólfur Orri <ingolfurorri@gmail.com>
2019-10-08 09:01:37 +0000

    Small changes

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,6 +1,17 @@
 pos_x = 1
 pos_y = 1
 
+
+
+def get_coin():
+    user_input = input("Pull a lever (y/n): ").lower()
+    if user_input == 'y':
+        return 1
+    else:
+        return 0
+
+
+
 def move_al(move_user, move_allowed):
     '''Checks if the move is legal'''
     move_user = move_user.lower()
@@ -9,6 +20,8 @@ def move_al(move_user, move_allowed):
             return True
 
     return False
+
+
 
 def movement_available(x, y):
     '''Checks the current position, and returns what moves are available'''
@@ -49,6 +62,8 @@ def movement_available(x, y):
 
     return movement, move_yes
 
+
+
 def move(x, y, move_user):
     '''Updates the position of the user according to input'''
     movement = move_user.lower()
@@ -63,10 +78,13 @@ def move(x, y, move_user):
 
     return x, y
 
+
+
 #Main program
 
 pos_current_x = pos_x
 pos_current_y = pos_y
+coins = 0
 
 while not (pos_current_x == 3 and pos_current_y == 1):
 
@@ -90,5 +108,7 @@ while not (pos_current_x == 3 and pos_current_y == 1):
 
     pos_current_x, pos_current_y = move(pos_current_x, pos_current_y, move_user)
 
+    coins += coin_check(list_of_coins, pos_current_x, pos_current_y)
+
 else:
-    print("Victory!")
+    print("Victory! Total coins {}".format(coins))
